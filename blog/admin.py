628c93b0e1c844138ea8c5cbc38dec1706e72d98
raimbaev223 <raimbaev.223@gmail.com>
2021-03-12 09:25:11 +0600

    qwe

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
-
 from .models import Post
 
 
-class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = '__all__'
-
-
-admin.site.register(Post, PostAdmin)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish',   
+                       'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
